c1924178c6a2a3cf84d8769111468de73ad3d7fa
na4zagin3 <mrty.ityt.pt@gmail.com>
2017-11-16 22:39:09 +0900

    Convert part-b.tex to part-b.yaml

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 pushd convert-multilingual-doc
 stack build
 stack exec -- convert-multilingual-doc < ../part-a.yaml > ../part-a.tex
+stack exec -- convert-multilingual-doc < ../part-b.yaml > ../part-b.tex
 popd
 
 latexmk -xelatex SprechenSieAttisch.tex
